7301c272075d9d56986cfd7a3a0e087f8c91654b
Jaime Albornoz <albornozsotojaime@gmail.com>
2023-01-16 14:06:32 -0300

    add login page

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
+import SignIn from './SignIn'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    {/* <App /> */}
+    <SignIn />
   </React.StrictMode>,
 )
